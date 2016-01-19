260a1e90e893fefabe64c363e1ba174fd763ccb8
aklein <aaronrklein@gmail.com>
2016-01-18 21:01:38 -0800

    Adding some level details and buildings

---
+++ __MAIN__/ARPG/Assets/NPC_Blacksmith_Greeting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_Blacksmith_Greeting : MonoBehaviour {
+
+    private Animator animator = null;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.gameObject.name);
+        if (col.gameObject.tag == "Player")
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("Greetings");
+            }
+        }
+    }
+}
